59027477ce34168e6d0bb0258535a4ad6e6d6206
Joseph <joemunga329@gmail.com>
2023-06-15 08:26:26 -0700

    msdk

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+* main - the begining of the program that prints a line using printf function.
+* Return: returns 0
+*/
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
