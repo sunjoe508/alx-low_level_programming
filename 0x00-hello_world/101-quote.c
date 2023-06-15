63be5a1b22b1221e4db603df47eb91a146649f2e
Joseph <joemunga329@gmail.com>
2023-06-15 12:51:32 -0700

    man of war

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+* main - the start of a program that outputs a line with standard error
+* Return: reurns 1
+*/
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
