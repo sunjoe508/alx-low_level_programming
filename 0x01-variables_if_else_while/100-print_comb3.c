085867b6a5631bdb1d616348e27581a723fc0fc7
Joseph <joemunga329@gmail.com>
2023-06-16 13:51:54 -0700

    master

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,10 +12,15 @@ for (m = '0'; m <= '9'; m++)
 {
 	for (n = '0'; n <= '9'; n++)
 	{
+		if(!((n ==m) || (m > n)))
+		{
+			putchar(m);
+			putchar(n);
 		if (!(n == '9' && m == '8'))
 		{
 			putchar(',');
 			putchar(' ');
+		}
 		}
 	}
 }
