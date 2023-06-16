e0e60f5b63b3905709d8f77036092708f23c3b3f
Joseph <joemunga329@gmail.com>
2023-06-16 07:18:17 -0700

    mahn 8

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+*main - the begining of the program
+*Return: returns 0
+*/
+
+int main(void)
+{
+int i = '0';
+while (i <= '9')
+{
+	putchar(i);
+	if (i != '9')
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	++i;
+}
+putchar('\n')
+return (0);
+}
