1a4d5d3109be7c79b43fff1409c2f6c691e0bbd2
Joseph <joemunga329@gmail.com>
2023-06-16 06:41:56 -0700

    master 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+*main - the begining of the program
+*Return: returns 0
+*/
+
+int main(void)
+{
+int num;
+while (num < 10)
+{
+printf("%d", num);
+num += 1;
+}
+printf("\n");
+return (0);
+}
