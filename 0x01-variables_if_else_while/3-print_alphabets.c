09a18149b13ab2014394bc2cdf81a2b11c1a1786
Joseph <joemunga329@gmail.com>
2023-06-16 06:06:16 -0700

    mass w

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+*main - the begining of the program
+*Return: returns 0
+*/
+
+int main(void)
+{
+int lowerCase = 'a';
+int upperCase = 'A';
+while (lowerCase <= 'z')
+{
+putchr(lowerCase);
+lowerCase += 1;
+}
+while (upperCase <= 'Z')
+{
+putchar(upperCase);
+upperCase += 1;
+}
+putchar('\n');
+retun (0);
+}
