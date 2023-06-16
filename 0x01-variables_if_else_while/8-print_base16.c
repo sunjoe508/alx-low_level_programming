396f276b0997e87fd9bf83b96a9046cc7258cf8e
Joseph <joemunga329@gmail.com>
2023-06-16 07:06:17 -0700

    mahn 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+*main - the begining of the program
+*Return: returns 0
+*/
+
+int main(void)
+{
+int num;
+char letter;
+
+for (num = 0; num < 10; num++)
+	putchar((num % 10) + '0');
+
+for (letter = 'a'; letter <= 'f'; letter++)
+	putchar(Letter);
+
+putchar('\n');
+
+return (0);
+}
+
