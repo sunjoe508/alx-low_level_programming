03e72372210996d8871287e0a3be33f77f285186
Joseph <joemunga329@gmail.com>
2023-06-16 06:32:50 -0700

    master 10

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+*main - the begining of the program
+*Return: returns 0
+*/
+
+int main(void)
+{
+int lowerCase = 'a';
+while (lowerCase <= 'z')
+{
+if (LowerCase == 'e' || lowerCase == 'q')
+{
+lowerCase += 1;
+}
+else
+{putchar(lowrCase);
+lowerCase += 1;
+}
+}
+putchar('\n');
+return (0);
+}
