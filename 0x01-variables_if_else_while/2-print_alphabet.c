cbcd28eae7c9a78cf6d5e6455fd206ba6616c9d7
Joseph <joemunga329@gmail.com>
2023-06-16 05:00:06 -0700

    mass w

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+* main - the begining of a program that prints the alphabet game
+* Return: will print 0 when the program is a success
+*/
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c < -'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
