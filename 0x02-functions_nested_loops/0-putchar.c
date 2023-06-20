e17a0ad6e65e16dd8feab58f5ca62a41f99cd976
Joseph <joemunga329@gmail.com>
2023-06-20 07:49:55 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - the program begins at this point
+ * the program is to print _putchar with out using printf function
+ * Return: the program returns 0  when successful
+ */
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
