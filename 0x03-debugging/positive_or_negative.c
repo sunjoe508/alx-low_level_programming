196da9141c2f6c225705b8f99a5424cbe5439847
Joseph <joemunga329@gmail.com>
2023-06-22 06:35:06 -0700

    massive

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - determiness whether the number is positive or negative
+ * Return: 0 onn success
+ */
+void positive_or_negative(int)
+{
+if (i < 0)
+{
+printf("%d is %s\n", i, "negative");
+}
+else if (i > 0)
+{
+printf("%d is %s\n", i, "positive");
+}
+else
+{
+printf("%d is %s\n", i, "zero");
+}
+return;
+
+}
