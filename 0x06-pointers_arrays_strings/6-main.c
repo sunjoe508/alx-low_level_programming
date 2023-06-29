511dbec7679c5162fb2d407a1d29ab1b6dc55af9
Joseph <joemunga329@gmail.com>
2023-06-29 13:22:09 -0700

    massive array

--- 0x06-pointers_arrays_strings/6-main.c
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -1,18 +1,18 @@
-nclude "main.h"
+#include "main.h"
 #include <stdio.h>
 
 /**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
- *     */
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	    char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
-	        char *ptr;
+    char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+    char *ptr;
 
-		    ptr = cap_string(str);
-		        printf("%s", ptr);
-			    printf("%s", str);
-			        return (0);
+    ptr = cap_string(str);
+    printf("%s", ptr);
+    printf("%s", str);
+    return (0);
 }
