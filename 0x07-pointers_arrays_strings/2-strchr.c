731aa32294bdc11b9eaf5613d45510479585a12f
Joseph <joemunga329@gmail.com>
2023-07-03 10:30:35 -0700

    master touch

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,17 +1,18 @@
 #include "main.h"
 /**
- * _strchr - this is the entry point
- * @s: the input
- * @c: the second input
- * Return: returns 0 when successful
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (success)
  */
 char *_strchr(char *s, char c)
 {
-int i = 0;
-for (; s[i] >= '\0'; i++)
-{
-if (s[i] == c)
-return (&s[i]);
-}
-return(0);
+	int i = 0;
+
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
 }
