4e6e91af480c3811787916af69d0eebb3408d0df
Joseph <joemunga329@gmail.com>
2023-07-17 13:56:44 -0700

    dog.h

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - this will free the memory allocated for a struct dog
+ * @d: the struct dog to free
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
