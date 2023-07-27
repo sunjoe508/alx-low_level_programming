d236fdbf26ab340a1c804b25668e403a0e31536a
joseph <joemunga329@gmail.com>
2023-07-27 13:42:54 -0700

    massive

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -4,7 +4,7 @@
  * add_node_end - adds new node at end
  * of a list_t list
  * @head: the head of linked list
- * @str: this is the string to store 
+ * @str: this is the string to store
  * in the list.
  * Return: returns the address of head.
  */
