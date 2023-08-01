e942aa79586d75b475e260b023e9d17dfb7de8d2
joseph <joemunga329@gmail.com>
2023-08-01 09:46:51 -0700

    mass list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * pop_listint - Should delete node in the list
+ * @head: points t first node of the list
+ * Return: The element that was deleted
+ * and 0 when empty
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *timp;
+	int tally;
+
+	if (head == NULL)
+		return (0);
+
+	tally = (*head)->n;
+	timp = (*head)->next;
+	free(*head);
+	*head = timp;
+
+	return (tally);
+}
