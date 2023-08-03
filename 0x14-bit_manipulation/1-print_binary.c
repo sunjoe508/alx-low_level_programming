b53090e0d3c1ea92a1fec1ef0f557be6b07c09bb
joseph <joemunga329@gmail.com>
2023-08-03 09:40:31 -0700

    bitmanipulation 1 and 2

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_binary - prints binary representation 
+ * @n: the number to print in binary form
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int timp;
+	int relocate;
+
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+
+	for (timp = n, relocate = 0; (timp >>= 1) > 0; relocate++)
+		;
+
+	for (; relocate >= 0; relocate--)
+	{
+		if ((n >> relocate) & 1)
+			printf("1");
+		else
+			printf("0");
+	}
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+here we learn about the opperators uste to manipulate data and its effective results 
